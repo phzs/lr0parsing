e01d4510e437ad6bbf80a9845270d7a1b8be10a7
phzs <zsph1011@hs-karlsruhe.de>
2018-08-08 10:32:19 +0200

    Fixes bug with initial window size and changes minimum window size

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,12 +2,19 @@ package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     private static final String APPLICATION_TITLE = "LR0 Parsing Visualization";
+    private static final double WINDOW_MIN_WIDTH = 800;
+    private static final double WINDOW_MIN_HEIGHT = 600;
+    private static final double WINDOW_SIZE_HORIZONTAL_PERCENT = 80;
+    private static final double WINDOW_SIZE_VERTICAL_PERCENT = 80;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -16,9 +23,19 @@ public class Main extends Application {
 
         primaryStage.setTitle(APPLICATION_TITLE);
         primaryStage.setScene(new Scene(root));
-        primaryStage.setMinWidth(800);
-        primaryStage.setMinHeight(800);
+        Point2D windowBounds = getWindowBounds();
+        primaryStage.setMinWidth(WINDOW_MIN_WIDTH);
+        primaryStage.setMinHeight(WINDOW_MIN_HEIGHT);
+        primaryStage.setWidth(windowBounds.getX());
+        primaryStage.setHeight(windowBounds.getY());
         primaryStage.show();
+    }
+
+    private Point2D getWindowBounds() {
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        double spaceH = (primaryScreenBounds.getWidth() / 100.0) * (100 - WINDOW_SIZE_HORIZONTAL_PERCENT);
+        double spaceV = (primaryScreenBounds.getHeight() / 100.0) * (100 - WINDOW_SIZE_VERTICAL_PERCENT);
+        return new Point2D(primaryScreenBounds.getWidth() - spaceH, primaryScreenBounds.getHeight() - spaceV);
     }
 
     public static void main(String[] args) {
